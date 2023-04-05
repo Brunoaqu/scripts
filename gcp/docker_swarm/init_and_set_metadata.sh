a55a0ba4b3cdbd2766c938c6610dd67725c74f95
Bruno Aquino <bruno.gl.aquino@gmail.com>
2023-04-05 14:00:40 -0300

    feat(gcp): added script to start swarm and add to metadata its token

--- gcp/docker_swarm/init_and_set_metadata.sh
+++ gcp/docker_swarm/init_and_set_metadata.sh
@@ -6,5 +6,16 @@ docker swarm init
 # Retrieve the join token for worker nodes
 token=$(docker swarm join-token -q worker)
 
+# Get the name of the current instance
+instance_name=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/name?alt=text)
+zone=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/zone?alt=text)
+
+# Extract just the zone name from the full URL
+zone=${zone##*/}
+
 # Set the metadata for the instance with the join token
-gcloud compute instances add-metadata $(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/name) --metadata swarm-join-token=$token
+gcloud compute instances add-metadata $instance_name --zone $zone --metadata swarm-join-token=$token
+
+echo "Swarm cluster initialized"
+echo "Join token for worker nodes: $token"
+echo "Instance metadata updated with join token"
